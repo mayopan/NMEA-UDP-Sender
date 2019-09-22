1d5b2a50bd9c95d0e3314186c822d275103e3d49
mayopan <49590865+mayopan@users.noreply.github.com>
2019-09-23 02:35:03 +0900

    Displaying status info added

--- src/NMEA-UDP-Sender.cpp
+++ src/NMEA-UDP-Sender.cpp
@@ -50,8 +50,8 @@ const int udpPort = 2947;
 const IPAddress ip(192, 168, 20, 1);
 const IPAddress subnet(255, 255, 255, 0);
 
-//Are we currently connected?
-boolean connected = false;
+//Are we currently wifiConnected?
+boolean wifiConnected = false;
 
 //The udp library class
 WiFiUDP udp;
@@ -73,65 +73,84 @@ void sendData(char *buff, bool SerialOut, bool UdpOut, bool OLEDOut);
 HMC5883L compass;
 HMC5883Compass compassData;
 
+boolean compassConnected = true;
+
 //End Compass section
 
 //OLED section
 #include <U8g2lib.h>
 U8G2_SH1107_PIMORONI_128X128_1_HW_I2C u8g2(U8G2_R0, /* reset=*/8);
+#define U8LOG_WIDTH 30
+#define U8LOG_HEIGHT 10
+uint8_t u8log_buffer[U8LOG_WIDTH * U8LOG_HEIGHT];
+U8G2LOG u8g2log;
 
 unsigned long lastTime = 0;                 //Simple local timer.
 const unsigned long Polling_Interval = 100; // ms = 10Hz It's not sampling interval. It should be faster than GPS sampling.
 
 void setup()
 {
+  uint8_t breakCounter = 0;
   u8g2.begin();
+  u8g2log.begin(u8g2, U8LOG_WIDTH, U8LOG_HEIGHT, u8log_buffer);
   u8g2.setFontMode(1);
-  u8g2.setFont(u8g2_font_crox2hb_tr);
-  u8g2.setCursor(0, 16);
-  //  u8g2.print("NMEA-UDP-Sender");
+  u8g2.setFont(u8g2_font_7x14_tr);
+  u8g2log.println("Booting.."); // output title
 
   Serial.begin(115200);
   while (!Serial)
-    ;   //Wait for user to open terminal
-        //  u8x8.print("Serial connected.");
-        //  u8x8.setCursor(0, 4);
+    ; //Wait for user to open terminal
+  u8g2log.println("Serial connected.");
   Serial.println("NMEA-UDP-Sender start.");
-  //Connect to the WiFi network
-  //  u8x8.print("Config Wifi AP");
-  //  u8x8.setCursor(0, 6);
-  Serial.println("Configuring access point...");
+
   //Create Access Point
+  u8g2log.println("Config Wifi AP");
+  Serial.println("Configuring access point...");
   WiFi.softAP(ssid, password);
   delay(100);
   WiFi.softAPConfig(ip, ip, subnet);
   IPAddress myIP = WiFi.softAPIP();
-  //  u8x8.print("AP IP address: ");
-  //  u8x8.setCursor(0, 8);
-  //  u8x8.print(myIP);
+  u8g2log.println("AP IP address: ");
+  u8g2log.println(myIP);
   Serial.print("AP IP address: ");
   Serial.println(myIP);
+  IPAddress broadcastIP = WiFi.softAPBroadcastIP();
+  u8g2log.println("Broadcast address: ");
+  u8g2log.println(broadcastIP);
 
   /*Initialize the mag sensor */
   while (!compass.begin())
   {
     Serial.println("Could not find a valid HMC5883L sensor, check wiring!");
     delay(500);
+    breakCounter++;
+    if (breakCounter > 4)
+    {
+      Serial.println("Compass function is disabled.");
+      u8g2log.println("Compass is NA.");
+      compassConnected = false;
+      break;
+    }
   }
+  if (compassConnected == true)
+  {
+    u8g2log.println("Compass Connected");
+    // Set measurement range
+    compass.setRange(HMC5883L_RANGE_1_3GA);
 
-  // Set measurement range
-  compass.setRange(HMC5883L_RANGE_1_3GA);
+    // Set measurement mode
+    compass.setMeasurementMode(HMC5883L_CONTINOUS);
 
-  // Set measurement mode
-  compass.setMeasurementMode(HMC5883L_CONTINOUS);
+    // Set data rate
+    compass.setDataRate(HMC5883L_DATARATE_30HZ);
 
-  // Set data rate
-  compass.setDataRate(HMC5883L_DATARATE_30HZ);
+    // Set number of samples averaged
+    compass.setSamples(HMC5883L_SAMPLES_8);
 
-  // Set number of samples averaged
-  compass.setSamples(HMC5883L_SAMPLES_8);
-
-  // Set calibration offset. See HMC5883L_calibration.ino
-  compass.setOffset(0, 0);
+    // Set calibration offset. See HMC5883L_calibration.ino
+    compass.setOffset(0, 0);
+    u8g2log.println("Compass config set.");
+  }
 
   //Assume that the U-Blox GPS is running at 9600 baud (the default) or at 115200 baud.
   //Loop until we're in sync and then ensure it's at 115200 baud.
@@ -147,7 +166,9 @@ void setup()
     Serial2.begin(9600);
     if (myGPS.begin(Serial2) == true)
     {
-      Serial.println("GPS: connected at 9600 baud, switching to 115200");
+      Serial.println("GPS: Connected at 9600 baud, switching to 115200");
+      u8g2log.println("GPS: Connected @9600");
+      u8g2log.println("Changing baud to 115200");
       myGPS.setSerialRate(115200);
       delay(400);
     }
@@ -159,18 +180,30 @@ void setup()
       delay(1000); //Wait a bit before trying again to limit the Serial output
     }
   } while (1);
+  Serial.println("GPS serial Connected @baud 115200");
+  u8g2log.println("GPS Connected.");
   udp.beginPacket(udpAddress1, udpPort);
-  Serial.println("GPS serial connected @baud 115200");
-  udp.print("GPS serial connected @baud 115200");
+  udp.print("GPS serial Connected @baud 115200");
   udp.endPacket();
-
+  Serial.print(F("Version: "));
+  u8g2log.print("Protcol Ver: ");
+  byte versionHigh = myGPS.getProtocolVersionHigh();
+  delay(1000);
+  Serial.print(versionHigh);
+  u8g2log.print(versionHigh);
+  Serial.print(".");
+  u8g2log.print(".");
+  byte versionLow = myGPS.getProtocolVersionLow();
+  delay(1000);
+  Serial.println(versionLow);
+  u8g2log.println(versionLow);
   myGPS.setUART1Output(COM_TYPE_UBX); //Set the UART port to output UBX only
   myGPS.setI2COutput(COM_TYPE_UBX);   //Set the I2C port to output UBX only (turn off NMEA noise)
   myGPS.setNavigationFrequency(5);
   myGPS.setAutoPVT(true, true);
   myGPS.saveConfiguration(); //Save the current settings to flash and BBR
-  delay(1000);
-  //u8x8.clear();
+  u8g2log.println("GPS config done.");
+  delay(10000);
 }
 
 void loop()
@@ -183,11 +216,15 @@ void loop()
     // When GPS has new solution, start encoding position data to NMEA sentences
     if (myGPS.getPVT())
     {
-      compassData.parse(compass);
+
       gpsData.parse(myGPS);
       sendData(gpsData.nmeaRMC, true, true, true);
       sendData(gpsData.nmeaGGA, true, true, false);
-      sendData(compassData.nmeaHDG, true, true, false);
+      if (compassConnected == true)
+      {
+        compassData.parse(compass);
+        sendData(compassData.nmeaHDG, true, true, false);
+      }
     }
   }
 }
@@ -240,10 +277,10 @@ void sendData(char *buff, bool SerialOut, bool UdpOut, bool OLEDOut)
       u8g2.drawTriangle(arrow_now_r[0].x, arrow_now_r[0].y, arrow_now_r[1].x, arrow_now_r[1].y, arrow_now_r[2].x, arrow_now_r[2].y);
       u8g2.drawCircle(center.x, center.y, 16);
       u8g2.setFont(u8g2_font_crox1hb_tr);
-      u8g2.drawGlyph(center.x - 4, center.y-15, 'N');
-      u8g2.drawGlyph(center.x - 4, center.y+16+8, 'S');
-      u8g2.drawGlyph(center.x +16, center.y+4, 'E');
-      u8g2.drawGlyph(center.x -16-8, center.y+4, 'W');
+      u8g2.drawGlyph(center.x - 4, center.y - 15, 'N');
+      u8g2.drawGlyph(center.x - 4, center.y + 16 + 8, 'S');
+      u8g2.drawGlyph(center.x + 16, center.y + 4, 'E');
+      u8g2.drawGlyph(center.x - 16 - 8, center.y + 4, 'W');
       for (int i = 0; i < 16; i++)
       {
         Vector2D startPt, endPt;
@@ -305,10 +342,9 @@ void sendData(char *buff, bool SerialOut, bool UdpOut, bool OLEDOut)
       //     u8g2.setFont(u8g2_font_open_iconic_check_2x_t);
       //      u8g2.drawGlyph(96, 128, 0x42);//out
       u8g2.setFont(u8g2_font_open_iconic_www_2x_t);
-      u8g2.drawGlyph(0, 128, 0x46);  //compass
-                                     //     u8g2.setCursor(16, 128);
-                                     //    u8g2.printf("%c%c%c", '\x46', '\x51', '\x4e'); //compass,wifi,,earth
-      u8g2.drawGlyph(88, 128, 0x47); //gps
+      u8g2.drawGlyph(0, 128, 0x46);  //position fix (arrow icon)
+      u8g2.drawGlyph(64, 128, 0x51); //Wifi
+      u8g2.drawGlyph(88, 128, 0x47); //sattelite in view (gps icon)
       u8g2.setFont(u8g2_font_crox3hb_tf);
       u8g2.setCursor(16, 128);
       switch (gpsData.fixtype) //0=no fix, 1=dead reckoning, 2=2D, 3=3D, 4=GNSS, 5=Time fix
@@ -333,6 +369,23 @@ void sendData(char *buff, bool SerialOut, bool UdpOut, bool OLEDOut)
         break;
       }
       u8g2.printf("%s", buff_u8g2);
+
+      u8g2.setFont(u8g2_font_open_iconic_app_2x_t);
+      u8g2.drawGlyph(40, 128, 0x46); //compass
+      u8g2.setFont(u8g2_font_crox3hb_tf);
+      u8g2.setCursor(56, 128);
+      if (compassConnected)
+      {
+        u8g2.print("o");
+      }
+      else
+      {
+        u8g2.print("x");
+      }
+
+      u8g2.setCursor(80, 128);
+      u8g2.print(WiFi.softAPgetStationNum());
+
       u8g2.setCursor(104, 128);
       u8g2.printf("%u", gpsData.SIV);
     } while (u8g2.nextPage());
